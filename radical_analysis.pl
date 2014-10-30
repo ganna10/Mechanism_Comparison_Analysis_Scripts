7bdd2f42b49af509eeb01cf767efc94950050fc1
ganna10 <janecoates10@gmail.com>
2014-10-30 15:05:27 +0100

    radical analyis improvements

--- radical_analysis.pl
+++ radical_analysis.pl
@@ -1,6 +1,7 @@
 #! /usr/bin/env perl
 # compare radical production as prescribed by NO source calculation
 # Version 0: Jane Coates 3/10/2014
+# Version 1: Jane Coates 30/10/2014 Merging OH+PAR and ROR, as they are tied together, plotting aesthetic improvements
 
 use strict;
 use diagnostics;
@@ -81,21 +82,20 @@ $R->run(q` library(ggplot2) `,
 
 $R->run(q` my.colours = c(  "Production Others" = "#696537",
                             "MGLYOX + hv" = "#f9c500", "CH3COCHO + hv" = "#f9c500", "CARB6 + hv" = "#f9c500", "MGLY + hv" = "#f9c500", 
-                            "NO + TCO3" = "#1c3e3d",
+                            "NO + TCO3" = "#603912",
                             "HC5 + OH" = "#c9a415",
                             "EPX + O3" = "#6db875",
                             "DCB1 + O3" = "#ae4901",
                             "BIGALD + hv" = "#8fd5d3",
                             "OH + PAR" = "#bb8a01", 
                             "C2O3 + NO" = "#e7e85e",
-                            "ROR" = "#0352cb", 
                             "C2H4 + OH" = "#9bb08f", 
-                            "CXO3 + NO" = "#ba8b01",
+                            "CXO3 + NO" = "#2c9daf",
                             "C2H6 + OH" = "#8ed6d2",
                             "O1D" = "#6c254f",
                             "HCHO + hv" = "#0e5c28", "CH2O + hv" = "#0e5c28", "FORM + hv" = "#0e5c28",
                             "DCB + hv" = "#76afca",
-                            "KET + hv" = "#a67c52",
+                            "KET + hv" = "#1c3e3d",
                             "CH4 + OH" = "#0d3e76",
                             "MEK + hv" = "#86b650" ) `,
 );
@@ -147,14 +147,14 @@ foreach my $run (sort keys %plot_data) {
 
 $R->run(q` CairoPDF(file = "radical_production_analysis.pdf", width = 141, height = 200) `,
         q` multiplot = grid.arrange(arrangeGrob(plots[[1]],
-                                                plots[[2]],
-                                                plots[[3]],
+                                                plots[[2]] + theme(axis.text.y = element_blank(), axis.ticks.y = element_blank()),
+                                                plots[[3]] + theme(axis.text.y = element_blank(), axis.ticks.y = element_blank()),
                                                 plots[[4]],
-                                                plots[[5]],
-                                                plots[[6]],
+                                                plots[[5]] + theme(axis.text.y = element_blank(), axis.ticks.y = element_blank()),
+                                                plots[[6]] + theme(axis.text.y = element_blank(), axis.ticks.y = element_blank()),
                                                 plots[[7]],
-                                                plots[[8]],
-                                                plots[[9]],
+                                                plots[[8]] + theme(axis.text.y = element_blank(), axis.ticks.y = element_blank()),
+                                                plots[[9]] + theme(axis.text.y = element_blank(), axis.ticks.y = element_blank()),
                                                 nrow = 3),
                                     nrow = 1, ncol = 1,
                                     left = textGrob(expression(bold(paste("Reaction Rate (molecules ", cm^-3, s^-1, ")"))), rot = 90, gp = gpar(fontsize = 140), vjust = 0.5)) `,
@@ -191,7 +191,6 @@ sub get_data {
     
     die "No producers found for $family\n" if (@$radical_producers == 0);
 
-    my $string_max = 30;
     for (0..$#$radical_producers) { #get rates for all radical producing reactions
         next if ($NOx_yields->[$_] < 0);
         my $reaction = $radical_producers->[$_];
@@ -202,51 +201,15 @@ sub get_data {
         my $rate = $net_radical_yield * $mecca->rate($reaction_number); 
         next if ($rate->sum == 0); # do not include reactions that do not occur 
         $reaction_string =~ s/_(.*?)\b//g;
-        $reaction_string =~ s/(.{1,$string_max})/$1\n/gs;
-        $reaction_string =~ s/\n$//;
-        $reaction_string = string_mapping($reaction_string);
         my ($reactants, $products) = split / = /, $reaction_string;
-        $production_rates{$reactants} += $rate(1:$NTIME-2);
-    }
-
-    sub string_mapping {
-        my ($string) = @_;
-        if ($string =~ /MGLYOX \+ hv/) {
-            $string = "MGLYOX + hv = CH3CO3 + CO\n+ HO2";
-        } elsif ($string =~ /NO \+ TCO3/) {
-            $string = "NO + TCO3 = 0.05 ACO3 + 0,95 CO\n+ 0.89 GLY + 0.92 HO2 + 0.11 MGLY\n+ NO2 + 2 XO2";
-        } elsif ($string =~ /HC5 \+ OH/) {
-            $string = "HC5 + OH = H2O + HC5P\n+ 0.25 XO2";
-        } elsif ($string =~ /EPX \+ O3/) {
-            $string = "EPX + O3 = 0.85 BALD + 1.5 CO\n+ 0.5 CO2 + GLY + 1.5 HO2 + 0.05 OH";
-        } elsif ($string =~ /DCB1 \+ O3/) {
-            $string = "DCB1 + O3 = 1.5 CO + 0.5 CO2 + 0.05 GLY\n+ 0.05 HCHO + HO2 + 0.08 MGLY + 0.05 OH\n+ 0.65 OP2 + 0.6 RCO3 + 0.6 XO2";
-        } elsif ($string =~ /CH3COCHO \+ hv/) {
-            $string = "CH3COCHO + hv = CH3CO3 + CO + HO2";
-        } elsif ($string =~ /BIGALD \+ hv/) {
-            $string = "BIGALD + hv = .13 CH3CO3 + .18 CH3COCHO\n+ .45 CO + .13 GLYOXAL + .56 HO2";
-        } elsif ($string =~ /OH \+ PAR = 0\.11 ALD2/) {
-            $string = "OH + PAR = 0.11 ALD2 + 0.11 HO2\n+ 0.11 PAROP + 0.76 ROR + 0.87 XO2 + 0.13 XO2N";
-        } elsif ($string =~ /C2O3 \+ NO/) {
-            $string = "C2O3 + NO = HCHO + HO2\n+ NO2 + XO2";
-        } elsif ($string =~ /ROR = 1\.1 ALD2/) {
-            $string = "ROR = 1.1 ALD2 + 0.94 HO2 + 2.1 PAROP\n+ 0.02 ROR + 0.96 XO2 + 0.04 XO2N";
-        } elsif ($string =~ /ETH \+ OH = 0\.22 ALD2/) {
-            $string = "C2H4 + OH = 0.22 ALD2 + 1.56 HCHO\n+ HO2 + XO2";
-        } elsif ($string =~ /OH \+ PAR = 0\.060 ALD2/) {
-            $string = "OH + PAR = 0.06 ALD2 + 0.05 ALDX + 0.11 HO2\n+ 0.11 PAROP + 0.76 ROR + 0.87 XO2 + 0.13 XO2N";
-        } elsif ($string =~ /ROR = 0\.600 ALD2/) {
-            $string = "ROR = 0.6 ALD2 + 0.5 ALDX + 0.94 HO2 + 2.1 PAROP\n+ 0.02 ROR + 0.96 XO2 + 0.04 XO2N";
-        } elsif ($string =~ /CXO3 \+ NO/) {
-            $string = "CXO3 + NO = ALD2 + HO2\n+ NO2 + XO2";
-        } elsif ($string =~ /ETH \+ OH = 0\.220 ALDX/) {
-            $string = "C2H4 + OH = 0.22 ALDX + 1.56 FORM\n+ HO2 + XO2";
-        } elsif ($string =~ /ETHA \+ OH/) {
-            $string = "C2H6 + OH = 0.991 ALD2 + HO2\n+ 0.991 XO2 + 0.009 XO2N";
-        } elsif ($string =~ /HCHO \+ hv = CO \+ 2\.0 HO2/) {
-            $string = "HCHO + hv = CO + 2 HO2";
+        if ($reactants eq "ROR") {
+            $reactants = "OH + PAR" ;
+        } elsif ($reactants eq "ETH + OH" and $family =~ /CB/) {
+            $reactants = "C2H4 + OH";
+        } elsif ($reactants eq "ETHA + OH") {
+            $reactants = "C2H6 + OH";
         }
-        return $string;
+        $production_rates{$reactants} += $rate(1:$NTIME-2);
     }
 
     my $others = 3e7;
