24ac072da1c23e6427e00cadc3c6746b78ebfc5d
ganna10 <janecoates10@gmail.com>
2014-11-07 22:21:19 +0100

    Re-plotted first day analysis as facetted plot

--- first_day_comparison.pl
+++ first_day_comparison.pl
@@ -1,13 +1,14 @@
 #! /usr/bin/perl
 # compare first day TOPP values of all VOCs from all mechanisms to MCM v3.2 TOPP values in a separate facet including y = x line
 # Version 0: Jane Coates 14/02/2014
+# Version 1: Jane Coates 7/11/2014 updating plot to include title of each facet rather than geom_text
 
 use strict;
 use diagnostics;
 use Statistics::R;
 
 my %TOPP;
-my $base = "/work/users/jco/MECCA/Mechanism_Comparison/TOPP_values_comparison";
+my $base = "/local/home/coates/MECCA/Mechanism_Comparison/TOPP_values_comparison";
 my $mcm3_1_file = "$base/MCMv3.1_TOPP_values.txt";
 my $mcm3_2_file = "$base/MCMv3.2_TOPP_values.txt";
 my $cri_file = "$base/CRIv2_TOPP_values.txt";
@@ -98,117 +99,74 @@ delete $TOPP{'XYO_RACM2'};
 $TOPP{'PXYL_RACM2'} = $TOPP{'XYP_RACM2'};
 delete $TOPP{'XYP_RACM2'};
 
-my $R = Statistics::R->new();
-$R->run(q` library(ggplot2) `);
-$R->run(q` library(plyr) `);
-$R->run(q` library(reshape2) `);
-$R->run(q` library(reshape) `);
-$R->run(q` library(grid) `);
-$R->run(q` library(gridExtra) `);
-$R->run(q` library(gtable) `);
-$R->run(q` library(Cairo) `);
-
-$R->run(q` NMVOC = {} `,
-        q` Mechanism = {} `,
-        q` TOPP.value = {} `,
-);
-
-foreach my $species (sort keys %TOPP) {
-    my ($name, $mechanism) = split /_/, $species;
-    my $R_name = $R->set('name', $name);
-    if ($mechanism =~ /MOZART/) {
-        my $R_mech = $R->set('mechanism', 'MOZART');
-    } elsif ($mechanism =~ /CBM/) {
-        my $R_mech = $R->set('mechanism', 'CBM4');
-    } else {
-        my $R_mech = $R->set('mechanism', $mechanism);
+my @mechanisms_fill = qw( MOZART-4 RADM2 RACM RACM2 );
+my @VOCs = map { local $_ = $_; s/_(.*?)$// ; $_ } grep { $_ =~ /MCMv3\.2/ } sort keys %TOPP;
+foreach my $mechanism (@mechanisms_fill) {
+    foreach my $VOC (@VOCs) {
+        my $label = "${VOC}_$mechanism";
+        $TOPP{$label} = 999 if (not defined $TOPP{$label});
     }
-    my $R_data = $R->set('TOPP', $TOPP{$species});
-    $R->run(q` NMVOC = cbind(NMVOC, name) `,
-            q` Mechanism = cbind(Mechanism, mechanism) `,
-            q` TOPP.value = cbind(TOPP.value, TOPP) `,
-    );
 }
 
-#create dataframe after converting the matrices above to vectors
-$R->run(q` NMVOC = c(NMVOC) `,
-        q` Mechanism = c(Mechanism) `,
-        q` TOPP.value = c(TOPP.value) `,
-        q` data = data.frame(NMVOC, Mechanism, TOPP.value) `,
-);
+my $R = Statistics::R->new();
+$R->run(q` library(ggplot2) `);
+$R->run(q` library(dplyr) `);
+$R->run(q` library(plyr) `);
+$R->run(q` library(reshape2) `);
+$R->run(q` library(grid) `);
+$R->run(q` library(Cairo) `);
 
-#reshape data frame and manually specify order of NMVOC
-$R->run(q` casted = cast(data, NMVOC ~ Mechanism, value = 'TOPP.value') `);
-$R->run(q` casted$NMVOC <- factor(casted$NMVOC, levels = c("C2H6", "C3H8", "NC4H10", "IC4H10", "NC5H12", "IC5H12", "NC6H14", "NC7H16", "NC8H18", "C2H4", "C3H6", "BUT1ENE", "MEPROPENE", "C5H8", "BENZENE", "TOLUENE", "MXYL", "OXYL", "PXYL", "EBENZ"), ordered = TRUE) `);
+$R->set('VOCs', [map { local $_ = $_; s/_(.*?)$// ; $_ } grep { $_ =~ /MCMv3\.2/ } sort keys %TOPP]);
+$R->run(q` data = data.frame(NMVOC = VOCs) `);
+
+my @mechanisms = ( "MCMv3.2", "(a) MCMv3.1",  "(b) CRIv2", "(c) RADM2", "(d) RACM", "(e) RACM2", "(f) MOZART-4", "(g) CBM-IV", "(h) CB05" );
+foreach my $mechanism (@mechanisms) { 
+    (my $true = $mechanism) =~ s/\([a-z]\)\s//;
+    $R->set('TOPPs', [ map { $TOPP{$_} } grep { $_ =~ /$true$/ } sort keys %TOPP]);
+    $R->set('mechanism', $mechanism);
+    $R->run(q` data[mechanism] = TOPPs `);
+}
+
+$R->run(q` data = melt(data, id.vars = c("NMVOC", "MCMv3.2"), variable.name = "Mechanism", value.name = "TOPP") `,
+        q` data = as.data.frame(lapply(data, function (x) {replace(x, x == 999, NA)})) `,
+);
+#my $p = $R->run(q` print(data) `);
+#print "$p\n";
 
 #specifiying colours and names
 $R->run(q` my.colours = c( "C2H6" = "#696537", "C3H8" = "#f9c600", "NC4H10" = "#76afca", "IC4H10" = "#dc3522", "NC5H12" = "#8c6238", "IC5H12" = "#9bb08f", "NC6H14" = "#8b1537", "NC7H16" = "#ba8b01", "NC8H18" = "#0352cb", "C2H4" = "#86b650", "C3H6" = "#6c254f", "BUT1ENE" = "#ee6738", "MEPROPENE" = "#58691b", "C5H8" = "#8ed6d5", "BENZENE" = "#f3aa7f", "TOLUENE" = "#c65d6c", "MXYL" = "#888a87", "OXYL" = "#0e5c28", "PXYL" = "#b569b3", "EBENZ" = "#2c9def" ) `,
         q` my.names = c("C2H6" = "Ethane", "C3H8" = "Propane", "NC4H10" = "Butane", "IC4H10" = "2-Methylpropane", "NC5H12" = "Pentane", "IC5H12" = "2-Methylbutane", "NC6H14" = "Hexane", "NC7H16" = "Heptane", "NC8H18" = "Octane", "C2H4" = "Ethene", "C3H6" = "Propene", "BUT1ENE" = "Butene", "MEPROPENE" = "2-Methylpropene", "C5H8" = "Isoprene", "BENZENE" = "Benzene", "TOLUENE" = "Toluene", "MXYL" = "m-Xylene", "OXYL" = "o-Xylene", "PXYL" = "p-Xylene", "EBENZ" = "Ethylbenzene") `,
+        q` data$NMVOC = factor(data$NMVOC, levels = c("C2H6", "C3H8", "NC4H10", "IC4H10", "NC5H12", "IC5H12", "NC6H14", "NC7H16", "NC8H18", "C2H4", "C3H6", "BUT1ENE", "MEPROPENE", "C5H8", "BENZENE", "TOLUENE", "MXYL", "OXYL", "PXYL", "EBENZ")) `,
 );
 
-$R->run(q` plot.lines = function () { list( geom_point(shape = 19, size = 55), 
-                                            xlab("\nMCM v3.2 TOPP (molecules(Ox)/molecules(VOC))"), 
-                                            scale_x_continuous(limits = c(0, 7), breaks = seq(0, 7, 1)), 
-                                            scale_y_continuous(limits = c(0,7), breaks = seq(0, 7, 1)), 
-                                            geom_abline(intercept = 0, slope = 1, size = 3), 
-                                            theme_bw(), 
-                                            theme(axis.text.x = element_text(size = 150), 
-                                            axis.text.y = element_text(size = 150), 
-                                            axis.title.x = element_text(size = 180, face = "bold"), 
-                                            axis.title.y = element_blank()), 
-                                            theme(panel.grid.minor.x=element_blank(), panel.grid.major.x=element_blank()) ,
-                                            theme(panel.grid.minor.y=element_blank(), panel.grid.major.y=element_blank()) ,
-                                            theme(axis.ticks.length = unit(.9, "cm")),
-                                            theme(axis.ticks.margin = unit(.8, "cm")),
-                                            theme(axis.ticks = element_line(size = 4)) ,
-                                            scale_colour_manual(labels = my.names, values = my.colours, guide = guide_legend(label.position = "bottom", label.theme = element_text(size = 170, angle = 45), label.hjust = 0.5, label.vjust = 0.9)), 
-                                            theme(legend.position = "bottom", legend.title = element_blank(), legend.key.size = unit(10, "cm"), legend.key = element_blank()) ) } `,
-); 
+$R->run(q` plot = ggplot(data, aes(x = MCMv3.2, y = TOPP, colour = NMVOC)) `,
+        q` plot = plot + geom_point(shape = 19, size = 55) `,
+        q` plot = plot + facet_wrap( ~ Mechanism, ncol = 2) `,
+        q` plot = plot + theme_bw() `,
+        q` plot = plot + xlab("\nMCM v3.2 TOPP (molecules(Ox)/molecules(VOC))\n") `,
+        q` plot = plot + ylab("TOPP (molecules(Ox)/molecules(VOC))\n") `,
+        q` plot = plot + geom_abline(intercept = 0, slope = 1, size = 5) `,
+        q` plot = plot + scale_x_continuous(limits = c(0, 7), breaks = seq(0, 7, 1)) `,
+        q` plot = plot + scale_y_continuous(limits = c(0, 7), breaks = seq(0, 7, 1)) `,
+        q` plot = plot + theme(strip.text = element_text(size = 200, face = "bold")) `,
+        q` plot = plot + theme(strip.background = element_blank()) `,
+        q` plot = plot + theme(axis.title.x = element_text(size = 180, face = "bold")) `,
+        q` plot = plot + theme(axis.title.y = element_text(size = 180, face = "bold")) `,
+        q` plot = plot + theme(panel.grid.major = element_blank()) `,
+        q` plot = plot + theme(panel.grid.minor = element_blank()) `,
+        q` plot = plot + theme(axis.text.x = element_text(size = 140)) `,
+        q` plot = plot + theme(axis.text.y = element_text(size = 140)) `,
+        q` plot = plot + theme(axis.ticks.length = unit(2, "cm")) `,
+        q` plot = plot + theme(axis.ticks.margin = unit(1, "cm")) `,
+        q` plot = plot + theme(legend.title = element_blank()) `,
+        q` plot = plot + theme(legend.key = element_blank()) `,
+        q` plot = plot + theme(legend.key.size = unit(7, "cm")) `,
+        q` plot = plot + theme(legend.position = "bottom") `,
+        q` plot = plot + scale_colour_manual(values = my.colours, labels = my.names, guide = guide_legend(label.position = "bottom", label.theme = element_text(size = 145, angle = 45), label.hjust = 0.5, label.vjust = 0.9)) `,
+);
 
-$R->run(q` mcm3.1.plot = ggplot(data = casted, aes(x = MCMv3.2, y = MCMv3.1, colour = NMVOC)) `,
-        q` mcm3.1.plot = mcm3.1.plot + plot.lines() `, 
-        
-        q` cri.plot = ggplot(data = casted, aes(x = MCMv3.2, y = CRIv2, colour = NMVOC)) `,
-        q` cri.plot = cri.plot + plot.lines() `, 
-        
-        q` mozart.plot = ggplot(data = casted, aes(x = MCMv3.2, y = MOZART, colour = NMVOC)) `,
-        q` mozart.plot = mozart.plot + plot.lines() `, 
-        
-        q` radm2.plot = ggplot(data = casted, aes(x = MCMv3.2, y = RADM2, colour = NMVOC)) `,
-        q` radm2.plot = radm2.plot + plot.lines() `, 
-        
-        q` racm.plot = ggplot(data = casted, aes(x = MCMv3.2, y = RACM, colour = NMVOC)) `,
-        q` racm.plot = racm.plot + plot.lines() `, 
-        
-        q` racm2.plot = ggplot(data = casted, aes(x = MCMv3.2, y = RACM2, colour = NMVOC)) `,
-        q` racm2.plot = racm2.plot + plot.lines() `, 
-        
-        q` cbm4.plot = ggplot(data = casted, aes(x = MCMv3.2, y = CBM4, colour = NMVOC)) `,
-        q` cbm4.plot = cbm4.plot + plot.lines() `, 
-        
-        q` cb05.plot = ggplot(data = casted, aes(x = MCMv3.2, y = CB05, colour = NMVOC)) `,
-        q` cb05.plot = cb05.plot + plot.lines() `, 
-        
-        q` legend = gtable_filter(ggplot_gtable(ggplot_build(mcm3.1.plot)), "guide-box") `,
-        q` y.label = textGrob("TOPP (molecules(Ox)/molecules(VOC))\n", rot = 90, gp = gpar(fontsize = 220, fontface = "bold"), vjust = 0.7) `,
-        q` CairoPDF(file = "first_day_values.pdf", width = 180, height = 200) `,
-        q` main.plot = grid.arrange(y.label, 
-                                    arrangeGrob(mcm3.1.plot + theme(legend.position="none", axis.title.x = element_blank(), axis.text.x = element_blank()) + annotate("text", x = 1, y = 6.5, label = "(a) MCM v3.1", size = 100, face = "bold") + scale_x_continuous(breaks = NULL), 
-                                                cri.plot + theme(legend.position="none", axis.title.x = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank()) + annotate("text", x = 1, y = 6.5, label = "(b) CRI v2", size = 100, face = "bold") + theme(axis.ticks = element_blank()), 
-                                                radm2.plot + theme(legend.position="none", axis.title.x = element_blank(), axis.text.x = element_blank()) + annotate("text", x = 1, y = 6.5, label = "(c) RADM2", size = 100, face = "bold") + scale_x_continuous(breaks = NULL), 
-                                                racm.plot + theme(legend.position="none", axis.title.x = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank()) + annotate("text", x = 1, y = 6.5, label = "(d) RACM", size = 100, face = "bold") + theme(axis.ticks = element_blank()), 
-                                                racm2.plot + theme(legend.position="none", axis.title.x = element_blank(), axis.text.x = element_blank()) + annotate("text", x = 1, y = 6.5, label = "(e) RACM2", size = 100, face = "bold") + scale_x_continuous(breaks = NULL), 
-                                                mozart.plot + theme(legend.position="none", axis.title.x = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank()) + annotate("text", x = 1, y = 6.5, label = "(f) MOZART-4", size = 100, face = "bold") + theme(axis.ticks = element_blank()), 
-                                                cbm4.plot + theme(legend.position="none") + annotate("text", x = 1, y = 6.5, label = "(g) CBM-IV", size = 100, face = "bold"), 
-                                                cb05.plot + theme(legend.position="none", axis.text.y = element_blank()) + annotate("text", x = 1, y = 6.5, label = "(h) CB05", size = 100, face = "bold") + theme(axis.ticks = element_blank()), 
-                                                ncol = 2), 
-                                    blank = grid.rect(gp = gpar(col = "white")), legend, 
-                                    ncol=2,
-                                    widths=unit.c(unit(26, "lines"), unit(1, "npc") - unit(26, "lines")),
-                                    nrow=2,
-                                    heights = c(175, 25)) `,
-
-        q` print(main.plot) `, 
+$R->run(q` CairoPDF(file = "first_day_values.pdf", width = 152, height = 200) `,
+        q` print(plot) `,
         q` dev.off() `,
 );
 
@@ -235,4 +193,4 @@ sub get_TOPPs {
         }
         close FILE;
     }
- }
+}
