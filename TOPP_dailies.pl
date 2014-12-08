1bdd465126cf571e84302cf8a85acac6b78c4c2e
ganna10 <janecoates10@gmail.com>
2014-12-08 15:57:18 +0100

    TOPP daily time series analysis

--- TOPP_dailies.pl
+++ TOPP_dailies.pl
@@ -2,252 +2,140 @@
 # print out daily TOPP values on one plot
 # Version 0: Jane Coates 10/02/2014
 # Version 1: Jane Coates 10/11/2014 aesthetic updates
+# Version 2: Jane Coates 8/12/2014 script updates after constant emissions runs
 
 use strict;
 use diagnostics;
 use Statistics::R;
 
 my %TOPP;
+my $base = "/local/home/coates/Documents/Analysis/2014_Mechanism_comparison_paper";
+opendir DIR, $base or die "Can't open $base : $!";
+my @daily_TOPP_files = grep { $_ =~ /_TOPP_values/ } readdir DIR;
+closedir DIR;
 
-my $base = "/local/home/coates/MECCA/Mechanism_Comparison/TOPP_values_comparison";
-my $mcm3_1_file = "$base/MCMv3.1_TOPP_values.txt";
-my $mcm3_2_file = "$base/MCMv3.2_TOPP_values.txt";
-my $cri_file = "$base/CRIv2_TOPP_values.txt";
-my $mozart_file = "$base/MOZART-4_TOPP_values.txt";
-my $radm2_file = "$base/RADM2_TOPP_values.txt";
-my $racm_file = "$base/RACM_TOPP_values.txt";
-my $racm2_file = "$base/RACM2_TOPP_values.txt";
-my $cbm4_file = "$base/CBM-IV_TOPP_values.txt";
-my $cb05_file = "$base/CB05_TOPP_values.txt";
-
-my @file_list = ($mcm3_1_file, $mcm3_2_file, $cri_file, $mozart_file, $radm2_file, $racm_file, $racm2_file, $cbm4_file, $cb05_file);
-get_TOPPs(@file_list);
-
-#re-assign species to MCM v3.2 species names
-$TOPP{'NC5H12_MOZART-4'} = $TOPP{'BIGALK_MOZART-4'};
-delete $TOPP{'BIGALK_MOZART-4'};
-$TOPP{'BUT1ENE_MOZART-4'} = $TOPP{'BIGENE_MOZART-4'};
-delete $TOPP{'BIGENE_MOZART-4'};
-$TOPP{'C5H8_MOZART-4'} = $TOPP{'ISOP_MOZART-4'};
-delete $TOPP{'ISOP_MOZART-4'};
-
-$TOPP{'C2H6_RADM2'} = $TOPP{'ETH_RADM2'};
-delete $TOPP{'ETH_RADM2'};
-$TOPP{'C3H8_RADM2'} = $TOPP{'HC3_RADM2'};
-delete $TOPP{'HC3_RADM2'};
-$TOPP{'NC5H12_RADM2'} = $TOPP{'HC5_RADM2'};
-delete $TOPP{'HC5_RADM2'};
-$TOPP{'NC8H18_RADM2'} = $TOPP{'HC8_RADM2'};
-delete $TOPP{'HC8_RADM2'};
-$TOPP{'C2H4_RADM2'} = $TOPP{'OL2_RADM2'};
-delete $TOPP{'OL2_RADM2'};
-$TOPP{'C3H6_RADM2'} = $TOPP{'OLT_RADM2'};
-delete $TOPP{'OLT_RADM2'};
-$TOPP{'MEPROPENE_RADM2'} = $TOPP{'OLI_RADM2'};
-delete $TOPP{'OLI_RADM2'};
-$TOPP{'C5H8_RADM2'} = $TOPP{'ISO_RADM2'};
-delete $TOPP{'ISO_RADM2'};
-$TOPP{'TOLUENE_RADM2'} = $TOPP{'TOL_RADM2'};
-delete $TOPP{'TOL_RADM2'};
-$TOPP{'MXYL_RADM2'} = $TOPP{'XYL_RADM2'};
-delete $TOPP{'XYL_RADM2'};
-
-$TOPP{'C2H6_RACM'} = $TOPP{'ETH_RACM'};
-delete $TOPP{'ETH_RACM'};
-$TOPP{'C3H8_RACM'} = $TOPP{'HC3_RACM'};
-delete $TOPP{'HC3_RACM'};
-$TOPP{'NC5H12_RACM'} = $TOPP{'HC5_RACM'};
-delete $TOPP{'HC5_RACM'};
-$TOPP{'NC8H18_RACM'} = $TOPP{'HC8_RACM'};
-delete $TOPP{'HC8_RACM'};
-$TOPP{'C2H4_RACM'} = $TOPP{'ETE_RACM'};
-delete $TOPP{'ETE_RACM'};
-$TOPP{'C3H6_RACM'} = $TOPP{'OLT_RACM'};
-delete $TOPP{'OLT_RACM'};
-$TOPP{'MEPROPENE_RACM'} = $TOPP{'OLI_RACM'};
-delete $TOPP{'OLI_RACM'};
-$TOPP{'C5H8_RACM'} = $TOPP{'ISO_RACM'};
-delete $TOPP{'ISO_RACM'};
-$TOPP{'TOLUENE_RACM'} = $TOPP{'TOL_RACM'};
-delete $TOPP{'TOL_RACM'};
-$TOPP{'MXYL_RACM'} = $TOPP{'XYL_RACM'};
-delete $TOPP{'XYL_RACM'};
-
-$TOPP{'C2H6_RACM2'} = $TOPP{'ETH_RACM2'};
-delete $TOPP{'ETH_RACM2'};
-$TOPP{'C3H8_RACM2'} = $TOPP{'HC3_RACM2'};
-delete $TOPP{'HC3_RACM2'};
-$TOPP{'NC5H12_RACM2'} = $TOPP{'HC5_RACM2'};
-delete $TOPP{'HC5_RACM2'};
-$TOPP{'NC8H18_RACM2'} = $TOPP{'HC8_RACM2'};
-delete $TOPP{'HC8_RACM2'};
-$TOPP{'C2H4_RACM2'} = $TOPP{'ETE_RACM2'};
-delete $TOPP{'ETE_RACM2'};
-$TOPP{'C3H6_RACM2'} = $TOPP{'OLT_RACM2'};
-delete $TOPP{'OLT_RACM2'};
-$TOPP{'MEPROPENE_RACM2'} = $TOPP{'OLI_RACM2'};
-delete $TOPP{'OLI_RACM2'};
-$TOPP{'C5H8_RACM2'} = $TOPP{'ISO_RACM2'};
-delete $TOPP{'ISO_RACM2'};
-$TOPP{'BENZENE_RACM2'} = $TOPP{'BEN_RACM2'};
-delete $TOPP{'BEN_RACM2'};
-$TOPP{'TOLUENE_RACM2'} = $TOPP{'TOL_RACM2'};
-delete $TOPP{'TOL_RACM2'};
-$TOPP{'MXYL_RACM2'} = $TOPP{'XYM_RACM2'};
-delete $TOPP{'XYM_RACM2'};
-$TOPP{'OXYL_RACM2'} = $TOPP{'XYO_RACM2'};
-delete $TOPP{'XYO_RACM2'};
-$TOPP{'PXYL_RACM2'} = $TOPP{'XYP_RACM2'};
-delete $TOPP{'XYP_RACM2'};
-
-my $R = Statistics::R->new();
-$R->run(q` library(ggplot2) `);
-$R->run(q` library(grid) `);
-$R->run(q` library(gridExtra) `);
-$R->run(q` library(gtable) `);
-$R->run(q` library(Cairo) `);
-
-#time axis
-my $n_days = 7;
-my @time_axis = (1..$n_days);
-$R->set('times', [@time_axis]); 
-$R->set('rep.number', scalar(keys %TOPP));
-
-$R->run(q` times = rep(times, rep.number) `,
-        q` NMVOC = {} `,
-        q` Mechanism = {} `,
-        q` TOPP.value = {} `,
-);
-
-foreach my $species (sort keys %TOPP) {
-    my ($name, $mechanism) = split /_/, $species;
-    my $R_name = $R->set('name', $name);
-    my $R_mech = $R->set('mechanism', $mechanism);
-    my $R_data = $R->set('TOPP', [@{$TOPP{$species}}]);
-    $R->run(q` NMVOC = cbind(NMVOC, rep(name, 7)) `,
-            q` Mechanism = cbind(Mechanism, rep(mechanism, 7)) `,
-            q` TOPP.value = cbind(TOPP.value, TOPP) `,
-    );
+foreach my $file (@daily_TOPP_files) {
+    my @lines = split /\n/, read_file($file);
+    (my $mechanism = $file) =~ s/^(.*?)_TOPP_values\.txt/$1/;
+    foreach my $line (@lines) {
+        next if ($line =~ /^Working|^CH4/);
+        my ($VOC, $TOPPs) = split / => /, $line;
+        $VOC = get_chemical_name($VOC);
+        $TOPPs =~ s/\s+/:/g;
+        my @TOPPs = split /:/, $TOPPs;
+        $TOPP{$mechanism}{$VOC} = \@TOPPs;
+    }
 }
 
-#create dataframe after converting the matrices above to vectors
-$R->run(q` NMVOC = c(NMVOC) `,
-        q` Mechanism = c(Mechanism) `,
-        q` TOPP.value = c(TOPP.value) `,
-        q` data = data.frame(times, NMVOC, Mechanism, TOPP.value) `,
+my $R = Statistics::R->new();
+$R->run(q` library(ggplot2) `,
+        q` library(tidyr) `,
+        q` library(Cairo) `,
+        q` library(grid) `,
 );
 
-#order facets by C-number
-$R->run(q` data$NMVOC = factor(data$NMVOC, levels = c('C2H6', 'C3H8', 'NC4H10', 'IC4H10', 'NC5H12', 'IC5H12', 'NC6H14', 'NC7H16', 'NC8H18', 'C2H4', 'C3H6', 'BUT1ENE', 'MEPROPENE', 'C5H8', 'BENZENE', 'TOLUENE', 'MXYL', 'PXYL', 'OXYL', 'EBENZ')) `);
-
-#change facet labels
-$R->run(q` facet_labels = list( "C2H6" = "Ethane\n",
-                                "C3H8" = "Propane\n",
-                                "NC4H10" = "Butane\n",
-                                "IC4H10" = "2-Methylpropane\n",
-                                "NC5H12" = "Pentane\n",
-                                "IC5H12" = "2-Methylbutane\n",
-                                "NC6H14" = "Hexane\n",
-                                "NC7H16" = "Heptane\n",
-                                "NC8H18" = "Octane\n",
-                                "C2H4" = "Ethene\n",
-                                "C3H6" = "Propene\n",
-                                "BUT1ENE" = "Butene\n",
-                                "MEPROPENE" = "2-Methylpropene\n",
-                                "C5H8" = "Isoprene\n",
-                                "BENZENE" = "Benzene\n",
-                                "TOLUENE" = "Toluene\n",
-                                "MXYL" = "m-Xylene\n",
-                                "PXYL" = "p-Xylene\n",
-                                "OXYL" = "o-Xylene\n",
-                                "EBENZ" = "Ethylbenzene\n") `
+$R->set('Time', [1..7]);
+$R->run(q` data = data.frame() `);
+foreach my $mechanism (sort keys %TOPP) {
+    $R->run(q` pre = data.frame(Time) `);
+    foreach my $VOC (sort keys %{$TOPP{$mechanism}}) {
+        $R->set('voc', $VOC);
+        $R->set('topps', [@{$TOPP{$mechanism}{$VOC}}]);
+        $R->run(q` pre[voc] = topps `);
+    }
+    $R->set('mechanism', $mechanism);
+    $R->run(q` pre$Mechanism = rep(mechanism, length(Time)) `,
+            q` pre = gather(pre, VOC, TOPP, -Time, -Mechanism) `,
+            q` data = rbind(data, pre) `,
+    );
+}
+#my $p = $R->run(q` print(data) `);
+#print $p, "\n";
+$R->run(q` my.colours = c("CB05" = "#0352cb", "CBM-IV" = "#b569b3", "CRIv2" = "#ef6638", "MCMv3.1" = "#000000", "MCMv3.2" = "#dc3522", "MOZART-4" = "#cc9900", "RACM" = "#6c254f", "RACM2" = "#4682b4", "RADM2" = "#035c28") `,
+        q` data$VOC = factor(data$VOC, levels = c("Ethane", "Propane", "Butane", "2-Methylpropane", "Pentane", "2-Methylbutane", "Hexane", "Heptane", "Octane", "Ethene", "Propene", "Butene", "2-Methylpropene", "Isoprene", "Benzene", "Toluene", "m-Xylene", "o-Xylene", "p-Xylene", "Ethylbenzene")) `,
 );
 
-#function for labeller
-$R->run(q` facet_labeller = function(variable,value) {return(facet_labels[value])} `); 
-$R->run(q` my.colours = c(  "CB05" = "#0352cb",
-                            "CBM-IV" = "#b569b3",
-                            "CRIv2" = "#ef6638", 
-                            "MCMv3.1" = "#000000",
-                            "MCMv3.2" = "#dc3522",
-                            "MOZART-4" = "#cc9900",
-                            "RACM" = "#6c254f",
-                            "RACM2" = "#4682b4",
-                            "RADM2" = "#035c28") `,);
-$R->run(q` plot.lines = function () { list( geom_line(size = 5) ,
-                                            geom_point(size = 10), 
-                                            scale_x_continuous(limits = c(1, 7), breaks = seq(1, 7, 1)), expand_limits(x = 1, y = 0), 
-                                            facet_grid(. ~ NMVOC, scales = "free", space = "free", labeller = facet_labeller), 
-                                            scale_y_continuous(limits = c(0,7), breaks = seq(0, 7, 1)), 
-                                            theme_bw(), 
-                                            theme(axis.text.x = element_text(size = 60)), 
-                                            theme(axis.text.y = element_text(size = 60)), 
-                                            theme(legend.title = element_blank()), 
-                                            theme(legend.text = element_text(size = 60)) ,
-                                            theme(legend.key.size = unit(6, "cm")), 
-                                            theme(strip.background = element_blank()), 
-                                            theme(strip.text.x = element_text(size = 70, face = "bold", vjust = 0)), 
-                                            theme(axis.title.x = element_blank()), 
-                                            theme(axis.title.y = element_blank()), 
-                                            scale_colour_manual(values = my.colours), 
-                                            theme(panel.grid.minor.x = element_blank(), panel.grid.major.x = element_blank()) ,
-                                            theme(axis.ticks.length = unit(0.5, "cm")) ,
-                                            theme(axis.ticks.margin = unit(0.25, "cm")) ,
-                                            theme(panel.grid.minor.y = element_blank(), panel.grid.major.y = element_blank()) ,
-                                            theme(panel.margin = unit(0.3 , "lines")), 
-                                            theme(legend.key = element_blank()) ) } `,
+$R->run(q` plot = ggplot(data, aes(x = Time, y = TOPP, colour = Mechanism, group = Mechanism)) `,
+        q` plot = plot + geom_point() `,
+        q` plot = plot + geom_line() `,
+        q` plot = plot + facet_wrap( ~ VOC ) `,
+        q` plot = plot + theme_bw() `,
+        q` plot = plot + xlab("Time (days)") `,
+        q` plot = plot + ylab("TOPP (molecules(Ox) / molecules(VOC))") `,
+        q` plot = plot + scale_x_continuous(limits = c(1, 7), breaks = seq(1, 7, 1)) `,
+        q` plot = plot + theme(axis.title = element_text(face = "bold")) `,
+        q` plot = plot + theme(strip.text = element_text(face = "bold")) `,
+        q` plot = plot + theme(strip.background = element_blank()) `,
+        q` plot = plot + theme(panel.grid = element_blank()) `,
+        q` plot = plot + theme(panel.border = element_rect(colour = "black")) `,
+        q` plot = plot + theme(legend.title = element_blank()) `,
+        q` plot = plot + theme(legend.key = element_blank()) `,
+        q` plot = plot + theme(legend.position = "bottom") `,
+        q` plot = plot + theme(plot.margin = unit(c(0.1, 0.1, 0.1, 0.1), "line")) `,
+        q` plot = plot + theme(legend.margin = unit(0, "lines")) `,
+        q` plot = plot + scale_colour_manual(values = my.colours, guide = guide_legend(nrow = 1)) `,
 );
 
-$R->run(q` plot1 = ggplot(data = subset(data, NMVOC == "C2H6" | NMVOC == "C3H8" | NMVOC == "NC4H10" | NMVOC == "IC4H10" | NMVOC == "NC5H12"), aes(x = times, y = TOPP.value, colour = Mechanism)) `,
-        q` plot1 = plot1 + plot.lines() `, 
-
-        q` plot2 = ggplot(data = subset(data, NMVOC == "IC5H12" | NMVOC == "NC6H14" | NMVOC == "NC7H16" | NMVOC == "NC8H18" | NMVOC == "C2H4"), aes(x = times, y = TOPP.value, colour = Mechanism)) `,
-        q` plot2 = plot2 + plot.lines() `, 
-
-        q` plot3 = ggplot(data = subset(data, NMVOC == "C3H6" | NMVOC == "BUT1ENE" | NMVOC == "MEPROPENE" | NMVOC == "C5H8" | NMVOC == "BENZENE"), aes(x = times, y = TOPP.value, colour = Mechanism)) `,
-        q` plot3 = plot3 + plot.lines() `, 
-
-        q` plot4 = ggplot(data = subset(data, NMVOC == "TOLUENE" | NMVOC == "MXYL" | NMVOC == "OXYL" | NMVOC == "PXYL" | NMVOC == "EBENZ"), aes(x = times, y = TOPP.value, colour = Mechanism)) `,
-        q` plot4 = plot4 + plot.lines() `, 
-
-        q` y.label = textGrob("TOPP (molecules(Ox)/molecules(VOC))\n", rot = 90, gp = gpar(fontsize = 70, fontface = "bold"), vjust = 0.8) `,
-        q` legend = gtable_filter(ggplot_gtable(ggplot_build(plot1)), "guide-box") `,
-                    q` CairoPDF(file = "TOPP_daily_values_all_species.pdf", width = 57, height = 40) `,
-                    q` main.plot = grid.arrange(y.label, 
-                                                arrangeGrob(plot1 + theme(legend.position="none", axis.text.x = element_blank(), axis.ticks.x = element_blank()), 
-                                                            plot2 + theme(legend.position="none", axis.text.x = element_blank(), axis.ticks.x = element_blank()), 
-                                                            plot3 + theme(legend.position="none", axis.text.x = element_blank(), axis.ticks.x = element_blank()), 
-                                                            plot4 + theme(legend.position="none"), 
-                                                            nrow = 4), 
-                                                sub = textGrob("Time (days)\n", gp = gpar(fontsize = 70, fontface = "bold"), vjust = 0.5), 
-                                                legend, 
-                                                nrow = 1, ncol = 3,
-                                                widths=unit.c(unit(8, "lines"), unit(1, "npc") - unit(8, "lines") - legend$width, legend$width)) `,
-                    q` print(main.plot) `,
-                    q` dev.off() `,
+$R->run(q` CairoPDF(file = "TOPP_daily_time_series_all_VOC.pdf", width = 8, height = 8) `,
+        q` print(plot) `,
+        q` dev.off() `,
 );
 
 $R->stop();
 
-sub get_TOPPs {
-    my (@files) = @_;
-    
-    foreach my $file (@files) {
-        my ($filename) = $file =~ m|comparison/(.*?)\.txt$|; 
-        my ($index, $extra) = split /_/, $filename;
-        open FILE, "<$file" or die $!;
-        foreach (<FILE>) {
-            next if ($_ =~ /^Working/ or $_ =~ /^CH4/); 
-            $_ =~ s/\[|\]//g;
-            my ($species, $TOPP) = split /=>/, $_;
-            foreach my $item ($species, $TOPP) {
-                $item =~ s/^\s+|\s+$//g;
-            }
-            $TOPP =~ s/\s+/,/g;
-            my @TOPP = split /,/, $TOPP;
-            $species .= "_$index"; 
-            $TOPP{$species} = \@TOPP;
-        }
-        close FILE;
+sub read_file {
+    my ($file) = @_;
+    open my $in, '<:encoding(utf-8)', $file or die "Can't open $file : $!";
+    local $/ = undef;
+    my $data = <$in>;
+    close $in;
+    return $data;
+}
+
+sub get_chemical_name {
+    my ($VOC) = @_;
+    my $chemical_species;
+    if ($VOC eq 'C2H6' or $VOC eq 'ETH') {
+        $chemical_species = 'Ethane';
+    } elsif ($VOC eq 'C3H8' or $VOC eq 'HC3') {
+        $chemical_species = 'Propane';
+    } elsif ($VOC eq 'NC4H10') {
+        $chemical_species = 'Butane';
+    } elsif ($VOC eq 'IC4H10') {
+        $chemical_species = '2-Methylpropane';
+    } elsif ($VOC eq 'NC5H12' or $VOC eq 'BIGALK' or $VOC eq 'HC5') {
+        $chemical_species = 'Pentane';
+    } elsif ($VOC eq 'IC5H12') {
+        $chemical_species = '2-Methylbutane';
+    } elsif ($VOC eq 'NC6H14') {
+        $chemical_species = 'Hexane';
+    } elsif ($VOC eq 'NC7H16') {
+        $chemical_species = "Heptane";
+    } elsif ($VOC eq 'NC8H18' or $VOC eq 'HC8') {
+        $chemical_species = "Octane";
+    } elsif ($VOC eq 'C2H4' or $VOC eq 'OL2' or $VOC eq 'ETE') {
+        $chemical_species = 'Ethene';
+    } elsif ($VOC eq 'C3H6' or $VOC eq 'OLT') {
+        $chemical_species = 'Propene';
+    } elsif ($VOC eq 'BUT1ENE' or $VOC eq 'BIGENE') {
+        $chemical_species = "Butene";
+    } elsif ($VOC eq 'MEPROPENE' or $VOC eq 'OLI') {
+        $chemical_species = '2-Methylpropene';
+    } elsif ($VOC eq 'C5H8' or $VOC eq 'ISO' or $VOC eq 'ISOP') {
+        $chemical_species = "Isoprene";
+    } elsif ($VOC eq 'BEN' or $VOC eq 'BENZENE') {
+        $chemical_species = "Benzene";
+    } elsif ($VOC eq 'TOLUENE' or $VOC eq 'TOL') {
+        $chemical_species = 'Toluene';
+    } elsif ($VOC eq 'MXYL' or $VOC eq 'XYM' or $VOC eq 'XYL') {
+        $chemical_species = "m-Xylene";
+    } elsif ($VOC eq 'OXYL' or $VOC eq 'XYO') {
+        $chemical_species = 'o-Xylene';
+    } elsif ($VOC eq 'PXYL' or $VOC eq 'XYP') {
+        $chemical_species = "p-Xylene";
+    } elsif ($VOC eq 'EBENZ') {
+        $chemical_species = "Ethylbenzene";
+    } else {
+        print "No chemical species found for $VOC\n";
     }
 }
