e54894bfbaa5fe0d130cc387943b5c3413418795
ganna10 <janecoates10@gmail.com>
2014-12-25 11:00:26 +0100

    MEPROPENE MOZART CH3CO3 and CH3CHO production budget

---
+++ trials/CH3CO3_production_MEPROPENE_degradation.pl
@@ -0,0 +1,198 @@
+#! /usr/bin/env perl
+# CH3CO3 production contributions during MEPROPENE degradation in MCM v3.2 and MOZART
+# Version 0: Jane Coates 18/11/2014
+
+use strict;
+use diagnostics;
+use MECCA;
+use KPP;
+use PDL;
+use PDL::NiceSlice;
+use Statistics::R;
+
+my $base = "/local/home/coates/MECCA";
+my $mecca = MECCA->new("$base/MCMv3.2_tagged/boxmodel");
+my $NTIME = $mecca->time->nelem;
+my $dt = $mecca->dt->at(0); 
+my $n_per_day = 43200 / $dt;
+my $n_days = int ($NTIME / $n_per_day);
+
+my @mechanisms = ( "MCMv3.2", "MOZART-4");
+my @species = qw( MEPROPENE BIGENE );
+my $index = 0;
+
+my (%families, %weights, %plot_data);
+foreach my $mechanism (@mechanisms) {
+    my $boxmodel = "$base/${mechanism}_tagged/boxmodel";
+    my $mecca = MECCA->new($boxmodel); 
+    my $eqnfile = "$base/${mechanism}_tagged/gas.eqn";
+    my $kpp = KPP->new($eqnfile); 
+    my $spcfile = "$base/${mechanism}_tagged/gas.spc";
+    my $all_tagged_species = get_tagged_species("CH3CO3", $spcfile); 
+    $families{$mechanisms[$index]} = [ @$all_tagged_species ];
+    ($plot_data{$mechanisms[$index]}) = get_data($kpp, $mecca, $mechanism, $species[$index]);
+    $index++;
+}
+
+my $R = Statistics::R->new();
+$R->run(q` library(ggplot2) `,
+        q` library(tidyr) `,
+        q` library(Cairo) `,
+);
+
+$R->set('Time', [("Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7")]);
+$R->run(q` data = data.frame() `);
+foreach my $mechanism (sort keys %plot_data) {
+    $R->run(q` pre = data.frame(Time) `);
+    foreach my $ref (@{$plot_data{$mechanism}}) {
+        foreach my $reaction (sort keys %$ref) {
+            $R->set('reaction', $reaction);
+            $R->set('rate', [map { $_ } $ref->{$reaction}->dog]);
+            $R->run(q` pre[reaction] = rate `);
+        }
+    }
+    $R->set('mechanism', $mechanism);
+    $R->run(q` pre$Mechanism = rep(mechanism, length(Time)) `,
+            q` pre = gather(pre, Reaction, Rate, -Time, -Mechanism) `, 
+            q` data = rbind(data, pre) `,
+    );
+}
+
+$R->run(q` plot = ggplot(data, aes(x = Time, y = Rate, fill = Reaction)) `,
+        q` plot = plot + geom_bar(stat = "identity") `,
+        q` plot = plot + facet_wrap( ~ Mechanism) `,
+);
+
+$R->run(q` CairoPDF(file = "MEPROPENE_CH3CO3_production_contributors.pdf") `,
+        q` print(plot) `,
+        q` dev.off() `,
+);
+
+$R->stop();
+
+sub get_data {
+    my ($kpp, $mecca, $species, $VOC) = @_;
+
+    my ($consumers, $producers, $consumer_yields, $producer_yields, %production_reaction_rates, %consumption_reaction_rates);
+    if (exists $families{$species}) { 
+        $kpp->family({ 
+                name    => $species,
+                members => $families{$species},
+                weights => $weights{$species},
+        });
+        $consumers = $kpp->consuming($species);
+        $producers = $kpp->producing($species);
+        $consumer_yields = $kpp->effect_on($species, $consumers); 
+        $producer_yields = $kpp->effect_on($species, $producers);  
+    } else {
+        print "No family found for $species\n";
+    }
+    die "No producers found for $species\n" if (@$producers == 0);
+    die "No consumers found for $species\n" if (@$consumers == 0);
+
+    my $prod_others_max = 8e5;
+    for (0..$#$producers) { #get rates for all producing reactions
+        my $reaction = $producers->[$_];
+        next unless ($reaction =~ /MEPROPENE|BIGENE/);
+        my $reaction_number = $kpp->reaction_number($reaction);
+        my $rate = $producer_yields->[$_] * $mecca->rate($reaction_number); 
+        $rate *= 0.65 if ($species eq "MOZART-4");
+        next if ($rate->sum == 0); # do not include reactions that do not occur 
+        my $reaction_string = $kpp->reaction_string($reaction);
+        $reaction_string =~ s/_(.*?)\b//g;
+        my ($reactants, $products) = split / = /, $reaction_string;
+        $production_reaction_rates{$reactants} += $rate(1:$NTIME-2);
+    }
+
+    for (0..$#$consumers) { #get rates for all consuming reactions
+        my $reaction = $consumers->[$_];
+        next unless ($reaction =~ /MEPROPENE|BIGENE/);
+        my $reaction_number = $kpp->reaction_number($reaction);
+        my $rate = $consumer_yields->[$_] * $mecca->rate($reaction_number); 
+        $rate *= 0.65 if ($species eq "MOZART-4");
+        next if ($rate->sum == 0); # do not include reactions that do not occur
+        my $reaction_string = $kpp->reaction_string($reaction);
+        $reaction_string =~ s/_(.*?)\b//g;
+        my ($reactants, $products) = split / = /, $reaction_string;
+        $consumption_reaction_rates{$reactants} += $rate(1:$NTIME-2);
+    } 
+
+    remove_common_processes(\%production_reaction_rates, \%consumption_reaction_rates);
+    foreach my $reaction (keys %production_reaction_rates) {
+        if ($production_reaction_rates{$reaction}->sum < $prod_others_max) {
+            $production_reaction_rates{"Production Others"} += $production_reaction_rates{$reaction};
+            delete $production_reaction_rates{$reaction};
+        }
+    }
+
+    foreach my $reaction (keys %production_reaction_rates) {
+        my $reshape = $production_reaction_rates{$reaction}->copy->reshape($n_per_day, $n_days);
+        my $integrate = $reshape->sumover;
+        $integrate = $integrate(0:13:2);
+        $production_reaction_rates{$reaction} = $integrate;
+    }
+    
+    my $sort_function = sub { $_[0]->sum };
+    my @prod_sorted_data = sort { &$sort_function($production_reaction_rates{$b}) <=> &$sort_function($production_reaction_rates{$a}) } keys %production_reaction_rates;
+    
+    my @final_sorted_data;
+    foreach (@prod_sorted_data) { 
+        next if ($_ eq 'Production Others') ;
+        push @final_sorted_data, { $_ => $production_reaction_rates{$_} };
+    } 
+    push @final_sorted_data, { 'Production Others' => $production_reaction_rates{'Production Others'} } if (defined $production_reaction_rates{'Production Others'}); 
+
+    return \@final_sorted_data;
+}
+
+sub remove_common_processes {
+    my ($production, $consumption) = @_;
+    my %common_processes;
+    $common_processes{$_} = 1 for (grep { defined $production->{$_} } keys %$consumption) ;
+
+    foreach my $process (keys %common_processes) {
+        my $net_effect = $production->{$process} + $consumption->{$process};
+        if ($net_effect->sum > 0) { #if net production remove consumption processes, retain net production
+            if (which($net_effect < 0)->nelem > 0) { #which gets indices of non-0 values, nelem get nr of elements
+                $production->{$process} .= $net_effect;
+                $production->{$process}->where($net_effect < 0) .= 0; #where returns output pdl corresponding to input pdl and meeting the condition
+                $consumption->{$process} .= $net_effect;
+                $consumption->{$process}->where($net_effect > 0) .= 0; #where returns output pdl corresponding to input pdl and meeting the condition
+                next;
+            } 
+            $production->{$process} = $net_effect;
+            delete $consumption->{$process};
+        } else { #net consumption
+            if (which($net_effect > 0)->nelem > 0) {
+                $production->{$process} .= $net_effect;
+                $production->{$process}->where($net_effect < 0) .= 0;
+                $consumption->{$process} .= $net_effect;
+                $consumption->{$process}->where($net_effect > 0) .= 0;
+                next;
+            }
+            $consumption->{$process} = $net_effect;
+            delete $production->{$process};
+        }
+    }
+}
+
+sub get_tagged_species {
+    my ($species, $spc_file) = @_;
+    my $all_species = read_file($spc_file);
+    my @tagged_species;
+    foreach my $line (@$all_species) {
+        next unless ($line =~ /^$species/);
+        $line =~ s/\s=.*$//;
+        push @tagged_species, $line;
+    }
+    return \@tagged_species;
+}
+
+sub read_file {
+    my ($file) = @_;
+
+    open my $in, '<:encoding(utf-8)', $file or die "Can't open $file for reading : $!";
+    chomp(my @all = <$in>);
+    close $in;
+    return \@all;
+} 
