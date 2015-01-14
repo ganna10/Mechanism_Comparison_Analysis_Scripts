f83504c14b77fce21307f3a5a2892b81ead0355f
ganna10 <janecoates10@gmail.com>
2015-01-14 23:05:22 +0100

    Finishing up Alkanes TOPP per C number analysis

--- trials/Ox_production_alkanes_per_carbon.pl
+++ trials/Ox_production_alkanes_per_carbon.pl
@@ -6,10 +6,13 @@ use strict;
 use diagnostics;
 use Statistics::R;
 
-my %TOPP;
+my (%TOPP, %sums);
 my $base = "/local/home/coates/Documents/Analysis/2014_Mechanism_comparison_paper/TOPP_plots";
 opendir DIR, $base or die "Can't open $base : $!";
 my @daily_TOPP_files = grep { $_ =~ /_TOPP_values/ } readdir DIR;
+closedir DIR;
+opendir DIR, $base or die "Can't open $base : $!";
+my @sums = grep { $_ =~ /_TOPP_cumulative\.txt/ } readdir DIR;
 closedir DIR;
 
 foreach my $file (@daily_TOPP_files) {
@@ -26,6 +29,19 @@ foreach my $file (@daily_TOPP_files) {
     }
 }
 
+foreach my $file (@sums) {
+    my $path = "$base/$file";
+    my @lines = split /\n/, read_file($path);
+    (my $mechanism = $file) =~ s/^(.*?)_TOPP_cumulative\.txt/$1/;
+    foreach my $line (@lines) {
+        next unless ($line =~ /ane/);
+        my ($VOC, $TOPPs) = split / => /, $line;
+        $TOPPs =~ s/\s+/:/g;
+        my @TOPPs = split /:/, $TOPPs;
+        $sums{$mechanism}{$VOC} = $TOPPs[-1];
+    }
+}
+
 my $R = Statistics::R->new();
 $R->run(q` library(ggplot2) `,
         q` library(tidyr) `,
@@ -33,6 +49,7 @@ $R->run(q` library(ggplot2) `,
 );
 
 $R->run(q` data = data.frame(Mechanism = as.numeric(0), VOC = as.numeric(0), TOPP.per.C = as.numeric(0)) `); 
+$R->run(q` sums = data.frame(Mechanism = as.numeric(0), VOC = as.numeric(0), TOPP.per.C = as.numeric(0)) `); 
 foreach my $mechanism (sort keys %TOPP) {
     $R->set('mechanism', $mechanism);
     foreach my $alkane (sort keys %{$TOPP{$mechanism}}) {
@@ -58,11 +75,41 @@ foreach my $mechanism (sort keys %TOPP) {
         $R->run(q` data = rbind(data, c(mechanism, alkane, topp.per.c)) `);
     } 
 }
+foreach my $mechanism (sort keys %sums) {
+    $R->set('mechanism', $mechanism);
+    foreach my $alkane (sort keys %{$sums{$mechanism}}) {
+        if ($alkane eq "Ethane") {
+            $sums{$mechanism}{$alkane} /= 2;
+        } elsif ($alkane eq "Propane") {
+            $sums{$mechanism}{$alkane} /= 3;
+        } elsif ($alkane eq "Butane" or $alkane eq "2-Methylpropane") {
+            $sums{$mechanism}{$alkane} /= 4;
+        } elsif ($alkane eq "Pentane" or $alkane eq "2-Methylbutane") {
+            $sums{$mechanism}{$alkane} /= 5;
+        } elsif ($alkane eq "Hexane") {
+            $sums{$mechanism}{$alkane} /= 6;
+        } elsif ($alkane eq "Heptane") {
+            $sums{$mechanism}{$alkane} /= 7;
+        } elsif ($alkane eq "Octane") {
+            $sums{$mechanism}{$alkane} /= 8;
+        } else {
+            print "No C for $alkane\n";
+        }
+        $R->set('alkane', $alkane);
+        $R->set('topp.per.c', $sums{$mechanism}{$alkane});
+        $R->run(q` sums = rbind(sums, c(mechanism, alkane, topp.per.c)) `);
+    } 
+}
 $R->run(q` data = data[-1,] `);
+$R->run(q` sums = sums[-1,] `);
 my $out = "Alkanes_TOPP_per_C.txt";
 my $p = $R->run(q` print(data) `);
 open my $file, ">:encoding(utf-8)", $out or die $!;
+print $file "First Day\n";
 print $file $p;
+print $file "\nCumulative\n";
+my $p1 = $R->run(q` print(sums) `);
+print $file $p1;
 close $file;
 
 $R->stop();
--- trials/alkanes_TOPP-per_C.R
+++ trials/alkanes_TOPP-per_C.R
@@ -1,7 +1,7 @@
 #R script to plot first day TOPP per carbon number vs alkane C number
 # Version 0: Jane Coates 5/1/2015
 
-#maximum TOPP for all alkanes
+#first day  TOPP for all alkanes
 cb05.data = data.frame(TOPP = c(0.0422586239874363, 0.121386567751567, 0.238201901316643, 0.238716972345505, 0.233166781832192, 0.233782935142518, 0.229173560937245, 0.225463160341231, 0.222113206982612), C = c(2, 3, 4, 4, 5, 5, 6, 7, 8), Mechanism = rep("CB05", 9), VOC = c("Ethane", "Propane", "Butane", "2-Methylpropane", "Pentane", "2-Methylbutane", "Hexane", "Heptane", "Octane"))
 cbm4.data = data.frame(TOPP = c(0.0434719733893872, 0.107176880041758, 0.210127040743828, 0.210603311657905, 0.20547103881836, 0.206040835380554, 0.201778173446655, 0.19834666076005, 0.195248052477836), C = c(2, 3, 4, 4, 5, 5, 6, 7, 8), Mechanism = rep("CBM-IV", 9), VOC = c("Ethane", "Propane", "Butane", "2-Methylpropane", "Pentane", "2-Methylbutane", "Hexane", "Heptane", "Octane"))
 cri.data = data.frame(TOPP = c(0.057076256722212, 0.118185053269068, 0.243190437555313, 0.241874828934669, 0.385513019561768, 0.31553225517273, 0.379698912302653, 0.339673059492897, 0.32830798625946), C = c(2, 3, 4, 4, 5, 5, 6, 7, 8), Mechanism = rep("CRIv2", 9), VOC = c("Ethane", "Propane", "Butane", "2-Methylpropane", "Pentane", "2-Methylbutane", "Hexane", "Heptane", "Octane"))
@@ -21,16 +21,6 @@ library(ggplot2)
 library(plyr)
 library(Cairo)
 
-lm_eqn = function(df){
-    m = lm(TOPP ~ C, df);
-    eq <- substitute(italic(TOPP) == a + b %.% italic(C)*","~~italic(r)^2~"="~r2, 
-         list(a = format(coef(m)[1], digits = 3), 
-              b = format(coef(m)[2], digits = 3), 
-             r2 = format(summary(m)$r.squared, digits = 4)))
-    as.character(as.expression(eq));
-}
-eqn = ddply(full, .(Mechanism), lm_eqn)
-
 my.colours = c( "Ethane" = "#696537", "Propane" = "#f9c600", "Butane" = "#76afca", "2-Methylpropane" = "#dc3522", "Pentane" = "#8c6238", "2-Methylbutane" = "#9bb08f", "Hexane" = "#8b1537", "Heptane" = "#ba8b01", "Octane" = "#0352cb" )
 
 plot = ggplot(full, aes(x = Mechanism, y = TOPP, colour = VOC))
@@ -38,7 +28,6 @@ plot = plot + geom_point()
 plot = plot + coord_flip()
 plot = plot + ylab("TOPP (molecules(Ox)/molecules(VOC)) per Carbon Number")
 plot = plot + scale_colour_manual(values = my.colours)
-#plot = plot + scale_x_continuous(limits = c(0, 0.4), breaks = seq(0, 0.4, 0.1))
 plot = plot + theme_bw()
 plot = plot + theme(axis.title = element_text(face = "bold"))
 plot = plot + theme(panel.border = element_rect(colour = "black"))
@@ -48,5 +37,37 @@ plot = plot + theme(legend.key = element_blank())
 plot = plot + theme(axis.title.y = element_blank())
 
 CairoPDF(file = "Alkanes_vs_C.pdf", width = 6, height = 4.2)
+print(plot)
+dev.off()
+
+#cumulative  TOPP for all alkanes
+cb05.sums = data.frame(TOPP = c(0.60335508361459, 0.923657203714053, 1.84673887491226, 1.84680387749839, 1.84610499031539, 1.84618226289749, 1.8456027507782, 1.84513571958547, 1.84471476078034), C = c(2, 3, 4, 4, 5, 5, 6, 7, 8), Mechanism = rep("CB05", 9), VOC = c("Ethane", "Propane", "Butane", "2-Methylpropane", "Pentane", "2-Methylbutane", "Hexane", "Heptane", "Octane"))
+cbm4.sums = data.frame(TOPP = c(0.257339833304286, 0.642852172255517, 1.28430606424809, 1.28446382284164, 1.28276613950729, 1.28295435905457, 1.28154492378235, 1.28041032181179, 1.27938625961543), C = c(2, 3, 4, 4, 5, 5, 6, 7, 8), Mechanism = rep("CBM-IV", 9), VOC = c("Ethane", "Propane", "Butane", "2-Methylpropane", "Pentane", "2-Methylbutane", "Hexane", "Heptane", "Octane"))
+cri.sums = data.frame(TOPP = c(0.90744724497199, 1.19261922438939, 2.02547438442707, 1.30156747996807, 2.15941095352172, 1.48752684593201, 2.19520086050033, 2.16198409962616, 2.15395130589604), C = c(2, 3, 4, 4, 5, 5, 6, 7, 8), Mechanism = rep("CRIv2", 9), VOC = c("Ethane", "Propane", "Butane", "2-Methylpropane", "Pentane", "2-Methylbutane", "Hexane", "Heptane", "Octane"))
+mcm1.sums = data.frame(TOPP = c(0.95420465618372, 1.24286951621373, 2.04508712887764, 1.33807555586099, 2.09400859475136, 1.63178697228432, 2.10139422118663, 2.09759336875754, 2.03783712722361), C = c(2, 3, 4, 4, 5, 5, 6, 7, 8), Mechanism = rep("MCMv3.1", 9), VOC = c("Ethane", "Propane", "Butane", "2-Methylpropane", "Pentane", "2-Methylbutane", "Hexane", "Heptane", "Octane"))
+mcm2.sums = data.frame(TOPP = c(0.865593794733285, 1.11035437385241, 1.9816093146801, 1.25637395679951, 2.0586550951004, 1.56194756031036, 2.07305474579333, 2.04698729770051, 1.97013106569648), C = c(2, 3, 4, 4, 5, 5, 6, 7, 8), Mechanism = rep("MCMv3.2", 9), VOC = c("Ethane", "Propane", "Butane", "2-Methylpropane", "Pentane", "2-Methylbutane", "Hexane", "Heptane", "Octane"))
+mozart.sums = data.frame(TOPP = c(0.88885911554098, 1.05732961495717, 1.66577024757862, 1.66577024757862, 1.66577024757862, 1.66577024757862, 1.66577024757862, 1.66577024757862, 1.66577024757862), C = c(2, 3, 4, 4, 5, 5, 6, 7, 8),Mechanism = rep("MOZART-4", 9), VOC = c("Ethane", "Propane", "Butane", "2-Methylpropane", "Pentane", "2-Methylbutane", "Hexane", "Heptane", "Octane"))
+radm2.sums = data.frame(TOPP = c(1.01430642907959, 1.7917226785901, 1.791722640909, 1.791722640909, 1.47968599696954, 1.47968599696954, 1.47968599696954, 1.47968599696954, 1.24018539668885), C = c(2, 3, 4, 4, 5, 5, 6, 7, 8),Mechanism = rep("RADM2", 9), VOC = c("Ethane", "Propane", "Butane", "2-Methylpropane", "Pentane", "2-Methylbutane", "Hexane", "Heptane", "Octane"))
+racm.sums = data.frame(TOPP = c(1.00219408422708, 1.82024583049204, 1.82024583049204, 1.82024583049204, 1.62936080495516, 1.62936080495516, 1.62936080495516, 1.62936080495516, 1.00811153726678), C = c(2, 3, 4, 4, 5, 5, 6, 7, 8), Mechanism = rep("RACM", 9), VOC = c("Ethane", "Propane", "Butane", "2-Methylpropane", "Pentane", "2-Methylbutane", "Hexane", "Heptane", "Octane"))
+racm2.sums = data.frame(TOPP = c(0.909124590456485, 1.42925610696828, 1.42925610696828, 1.42925610696828, 1.1370189424072, 1.1370189424072, 1.1370189424072, 1.1370189424072, 1.05259203890672), C = c(2, 3, 4, 4, 5, 5, 6, 7, 8), Mechanism = rep("RACM2", 9), VOC = c("Ethane", "Propane", "Butane", "2-Methylpropane", "Pentane", "2-Methylbutane", "Hexane", "Heptane", "Octane"))
+
+sum = rbind(cb05.sums, cbm4.sums, cri.sums, mcm1.sums, mcm2.sums, mozart.sums, radm2.sums, racm.sums, racm2.sums)
+sum$Mechanism = factor(sum$Mechanism, levels = rev(c("MCMv3.2", "MCMv3.1", "CRIv2", "RADM2", "RACM", "RACM2", "MOZART-4", "CBM-IV", "CB05")))
+sum$VOC = factor(sum$VOC, levels = c("Ethane", "Propane", "Butane", "2-Methylpropane", "Pentane", "2-Methylbutane", "Hexane", "Heptane", "Octane"))
+
+plot = ggplot(sum, aes(x = Mechanism, y = TOPP, colour = VOC))
+plot = plot + geom_point()
+plot = plot + coord_flip()
+plot = plot + ylab("TOPP (molecules(Ox)/molecules(VOC)) per Carbon Number")
+plot = plot + scale_colour_manual(values = my.colours)
+plot = plot + theme_bw()
+plot = plot + theme(axis.title = element_text(face = "bold"))
+plot = plot + theme(panel.border = element_rect(colour = "black"))
+plot = plot + theme(panel.grid = element_blank())
+plot = plot + theme(legend.title = element_blank())
+plot = plot + theme(legend.key = element_blank())
+plot = plot + theme(axis.title.y = element_blank())
+
+CairoPDF(file = "Alkanes_vs_C_sums.pdf", width = 6, height = 4.2)
 print(plot)
 dev.off()
